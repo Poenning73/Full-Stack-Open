44abc608c0b6d2a3555726e9c6d9ac365f287c17
Saku Pönni <saku.ponni@aalto.fi>
2025-12-15 20:43:37 +0200

    removed cors

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 morgan.token('post-content', (req, res) => JSON.stringify(req.body) )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'))
-app.use(cors())
 
 let persons = [
   {
